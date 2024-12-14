266deba4ae66f08c3647c96c8c1d439aa5027f02
ArnavJain0902 <arnavjain0902@gmail.com>
2024-12-14 19:28:10 +0530

    Initial Files

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import {CORE_CONCEPTS} from './data.js'
+import Header from './components/Header/Header.jsx'
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[1]}></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
+            <CoreConcept
+              title ={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description} 
+              image = {CORE_CONCEPTS[3].image}> 
+            </CoreConcept>
+          </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
