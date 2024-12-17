5d41fd8f230ad9b09d198ec266d489e4fde75890
ArnavJain0902 <arnavjain0902@gmail.com>
2024-12-17 20:33:49 +0530

    Added dynamic active classes to tab buttons

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,5 +1,5 @@
 
-export default function TabButton({children,onClick}){
+export default function TabButton({children,onClick,isSelected}){
 
-    return <li><button onClick={onClick}>{children}</button></li>
+    return <li><button className={isSelected ? 'active' :undefined} onClick={onClick}>{children}</button></li>
 }
