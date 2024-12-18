b54e2fed4ad5a8c88cacca1b9a9ba5baead17f54
ArnavJain0902 <arnavjain0902@gmail.com>
2024-12-18 17:22:32 +0530

    Splitted components across different files and used fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,17 @@
-import {CORE_CONCEPTS} from './data.js'
-import {EXAMPLES} from './data.js'
 import Header from './components/Header/Header.jsx'
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
-import {useState} from 'react' 
-
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 function App() {
-  const [selectedTopic,setSelectedTopic]=useState()
-  function handleClick(selectedButton){
-    // selectedButton => 'components','props','JSX','State'
-    setSelectedTopic(selectedButton)
-  }
 
-
-  let tabContent = (<p>Please select a Topic</p>)
-  if(selectedTopic){
-    tabContent = (<div id="tab-content">
-    <h3>{EXAMPLES[selectedTopic].title}</h3>
-    <p>{EXAMPLES[selectedTopic].description}</p>
-    <pre>
-      <code>
-        {EXAMPLES[selectedTopic].code}
-      </code>
-    </pre>
-  </div>)
-  }
-
-  
   return (
-    <div>
+    <>
       <Header></Header>
       <main>
-        <section id='core-concepts'>
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem)=><CoreConcept key={conceptItem.title} {...conceptItem}></CoreConcept>)}
-          </ul>
-        </section>
-        <section id='examples'>
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onClick={()=>handleClick('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'}  onClick={()=>handleClick('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onClick={()=>handleClick('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onClick={()=>handleClick('state')}>State</TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
