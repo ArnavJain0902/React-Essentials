db5c143c3fc4f9fc27c76ea8841cb81ab552f9f7
ArnavJain0902 <arnavjain0902@gmail.com>
2024-12-18 21:10:55 +0530

    Made Tabs and Section reusable

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,7 +1,8 @@
 import TabButton from "./TabButton"
 import { useState } from "react"
 import { EXAMPLES } from "../data"
-
+import Section from "./Section"
+import Tabs from "./Tabs"
 export default function Examples(){
     const [selectedTopic,setSelectedTopic]=useState()
     function handleClick(selectedButton){
@@ -23,15 +24,24 @@ export default function Examples(){
     }
     
     return(
-        <section id='examples'>
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onClick={()=>handleClick('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'}  onClick={()=>handleClick('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onClick={()=>handleClick('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onClick={()=>handleClick('state')}>State</TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <Section id='examples' title='Examples'>
+          <h2></h2>
+          <Tabs 
+            buttons={
+              <>
+                <menu>
+                  <TabButton isSelected={selectedTopic === 'components'} onClick={()=>handleClick('components')}>Components</TabButton>
+                  <TabButton isSelected={selectedTopic === 'jsx'}  onClick={()=>handleClick('jsx')}>JSX</TabButton>
+                  <TabButton isSelected={selectedTopic === 'props'} onClick={()=>handleClick('props')}>Props</TabButton>
+                  <TabButton isSelected={selectedTopic === 'state'} onClick={()=>handleClick('state')}>State</TabButton>
+                </menu>
+              </>
+            }
+          >
+            {tabContent}
+          </Tabs>
+         
+          
+        </Section>
     )
 }
